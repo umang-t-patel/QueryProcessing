3bd03dd3228d30b58b0a59d1c105c1d40ebdd0c6
Umang Patel <umangtpatel@hotmail.com>
2017-06-17 01:01:51 -0400

    Updating for entity type

--- testqueryprocessing/exago.py
+++ testqueryprocessing/exago.py
@@ -6,6 +6,7 @@ googleapikey = 'https://language.googleapis.com/v1/documents:annotateText?fields
 sessionurl = 'http://exagosupport.com:85/Exago/NLP_REST_API/WebServiceApi/Rest/Sessions'
 queryurl = 'http://exagosupport.com:85/Exago/NLP_REST_API/WebServiceApi/Rest/Reports/execute/csv?sid={0}'
 header = {'Content-Type': 'application/json','Accept': 'application/json','Authorization': 'Basic Og=='}
+entities_type = {}
 def googlettsapi(text):
     data = {"content": text,"type": "PLAIN_TEXT"}
     request_document = {}
@@ -15,7 +16,8 @@ def googlettsapi(text):
     response = requests.post(url = googleapikey,
     	data = json.dumps(request_document),
     	headers = {'Content-Type': 'application/json'})
-    # print(response.json())
+    global entities_type
+    entities_type = response.json()['entities']
     temp_speech_text = response.json()['tokens']
     speech_ret_text = []
     print('Removing Determiner')
@@ -67,18 +69,14 @@ def performNLP(google_output):
     print('Non-Noun')
     result_text_noun = [text['text'] for text in google_output if text['partOfSpeech']['tag'] == 'NOUN']
 
-    # print('Not Noun:',result_text_not_noun)
-    # print('Noun:',result_text_noun)
     print('Getting List of Table Name')
     table_data = [key for key, value in exago_data.table_data.items()]
     print('Perform Similarity for Table Name')
     table_output = performsimilarity(result_text_noun,table_data)
     attr_data = []
-    # [item for sublist in l for item in exago_data.table_data.items()]
-    # list(itertools.chain.from_iterable(a))
     print('Getting List of Attributes')
     attr_data = itertools.chain.from_iterable(exago_data.table_data.values())
-    # print('attr data:',attr_data)
+
     print('Perform Similarity for Attributes')
     attr_output = performsimilarity(result_text_noun,attr_data)
 
@@ -92,9 +90,7 @@ def performNLP(google_output):
     print('Getting Similarity of Opertional Data')
     opr_output = performsimilarity(result_text_not_noun,opr_data)
 
-    # print('table data:',table_data)
     print('table output:',table_output)
-    # print('attr data:',attr_data)
     print('Attributes output:',attr_output)
     print('Aggregation output:',agg_output)
     print('Opertional output:',opr_output)
@@ -111,27 +107,31 @@ def performsimilarity(input_list, data_list):
     url = settings.NLTK_DIR
     nltk.data.path.append(url)
     output_list = []
+    global entities_type
     for input_word in input_list:
         for data_word in data_list:
             wordFrominput_list = wordnet.synsets(input_word)
             wordFromdata_list = wordnet.synsets(data_word)
-            if wordFrominput_list and wordFromdata_list: #Thanks to @alexis' note
+            if wordFrominput_list and wordFromdata_list:
                 for wordfromdata in wordFromdata_list:
                     s = wordFrominput_list[0].wup_similarity(wordfromdata)
                     if s == None:
                         s = 0.0
                     output_list.append([s, input_word, data_word])
+            elif [dic_entity['type'] for dic_entity in entities_type if input_word in dic_entity.values()]:
+                entity_type = [dic_entity['type'] for dic_entity in entities_type if input_word in dic_entity.values()]
+                wordFrominput_list = wordnet.synsets(entity_type[0])
+                wordFromdata_list = wordnet.synsets(data_word)
+                if wordFrominput_list and wordFromdata_list:
+                    for wordfromdata in wordFromdata_list:
+                        s = wordFrominput_list[0].wup_similarity(wordfromdata)
+                        if s == None:
+                            s = 0.0
+                        output_list.append([s, input_word, data_word])
     return max(output_list)
 def getresults(text):
     print('Get Results from Google NLP')
     google_output = googlettsapi(text)
     print('Passing Results from Google NLP to perform NLP')
     nlp_output = performNLP(google_output)
-    # send_data = {"DisplayField": "Employees_0.City","AggMethod": 6}
-    # temp_data = [{"EntityName": "Employees_0", "ColumnName": "LastName", "Operator": 0, "Values": [ "King", "null" ]}]
-    # # temp_data = [{"EntityName": "Employees_0","ColumnName": "LastName","Operator": 0,"Values": [ "Davolio", "null" ]},
-    # # {"EntityName": "Products_0","ColumnName": "ProductName","Operator": 0,"Values": [ "Tofu", "null" ]},
-    # # {"EntityName": "Orders_0","ColumnName": "OrderDate","Operator": 12,"Values": [ "2014-01-01 00:00:00.000", "2014-12-31 00:00:00.000"]}]
-    # send_data["Filters"] = temp_data
-    # print(getdata(send_data)["ExecuteData"])
     return google_output,nlp_output
